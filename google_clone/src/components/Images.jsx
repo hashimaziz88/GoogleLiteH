972020d0009ac9813cc5a0e571a44b0aa7e1db33
Hashim Aziz Muhammad <haazizjhb024@student.wethinkcode.co.za>
2024-11-15 11:57:50 +0200

    adds

--- google_clone/src/components/Images.jsx
+++ google_clone/src/components/Images.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import "./style/Images.css";
 
 const Images = () => {
   return (
     <div>
       <h1>Images</h1>
+      <div className="container">
+        <div className="center">
+          <img src="https://images.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png" />
+
+          <span>images</span>
+        </div>
+      </div>
     </div>
   );
 };
